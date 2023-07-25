92dfc80cd32c38fc0c6bcfa7d625425d042fcc71
ukssss <1872003115su@gmail.com>
2023-07-25 23:43:00 +0900

    ADD | Font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Inter } from 'next/font/google';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const inter = Inter({
+    subsets: ['latin'],
+});
+
+function App({ Component, pageProps }: AppProps) {
+    return (
+        <div className={inter.className}>
+            <Component {...pageProps} />;
+        </div>
+    );
 }
+
+export default App;
